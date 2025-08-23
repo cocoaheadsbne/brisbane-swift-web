500ce0a237efbd75ac2f3f752bb6adc4d731880a
Joschua <70809675+selfire1@users.noreply.github.com>
2025-08-23 14:31:57 +1000

    formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-
 import icon from "astro-icon";
-
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
 
   vite: {
