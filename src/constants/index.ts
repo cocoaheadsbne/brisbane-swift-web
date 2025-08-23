1d479e4270a9c763b43d9a933819476bc349465d
Joschua <70809675+selfire1@users.noreply.github.com>
2025-08-23 14:22:13 +1000

    formatting

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,8 +3,8 @@ export enum ROUTES {
   ABOUT = "/about",
 }
 
-  export enum HREFS {
-MEETUPCOM = "https://www.meetup.com/en-AU/brisbane-swift/",
+export enum HREFS {
+  MEETUPCOM = "https://www.meetup.com/en-AU/brisbane-swift/",
   SLACK_INVITE = "https://join.slack.com/t/cocoaheads-aunz/shared_invite/zt-2ros1omdt-CSPgMo2~Gq7hXXXiGIi7qw",
-  MANTEL_GROUP = "https://mantelgroup.com.au/"
-  }
+  MANTEL_GROUP = "https://mantelgroup.com.au/",
+}
